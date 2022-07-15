8cef5de6d5348fefef4d8c2e0666998413cbb9ec
Paul Sonnentag <paul.sonnentag@gmail.com>
2022-07-15 16:06:21 +0200

    Add simple formula editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import {Editor, EDITOR_VIEW, getAllSortedSnippets, getParserOfType, setIsInDragMode} from "./Editor";
-import {Snippet, Span, textEditorStateMobx} from "./primitives";
-import {observer} from "mobx-react-lite";
-import {useState} from "react";
-import {AdjacentTokenRelationshipType, Column, findMatches, inferRelationships, Match} from "./rules";
-import {nanoid} from "nanoid";
-import { run } from "./formulas";
+import { Editor, EDITOR_VIEW, getAllSortedSnippets, getParserOfType, setIsInDragMode } from "./Editor";
+import { Snippet, Span, textEditorStateMobx } from "./primitives";
+import { observer } from "mobx-react-lite";
+import { useState } from "react";
+import { AdjacentTokenRelationshipType, Column, findMatches, inferRelationships, Match } from "./rules";
+import { nanoid } from "nanoid";
+import { evaluateColumns, FormulaColumn, ResultRow } from "./formulas";
 
 
 export const Table = observer(() => {
@@ -39,12 +39,12 @@ export const Table = observer(() => {
       example: snippet
     }))
 
-    EDITOR_VIEW.dispatch({ effects: setIsInDragMode.of(false)})
+    EDITOR_VIEW.dispatch({ effects: setIsInDragMode.of(false) })
   }
 
-  const changeColumnNameAt = (changedIndex : number, name: string) => {
+  const changeColumnNameAt = (changedIndex: number, name: string) => {
     setColumns(columns.map((column, index) => (
-      index === changedIndex ? { ... column, name } : column
+      index === changedIndex ? { ...column, name } : column
     )))
   }
 
@@ -58,9 +58,6 @@ export const Table = observer(() => {
   const sortedSnippets = getAllSortedSnippets(doc.sliceString(0))
 
 
-  run(sortedSnippets, doc)
-
-
   const matches: Match[] = findMatches(columns, relationships, sortedSnippets)
 
   return (
@@ -72,7 +69,7 @@ export const Table = observer(() => {
             {columns.map((column, index) => {
               return (
                 <th key={index} className="bg-gray-100 border border-gray-200">
-                  <input className="px-1 bg-gray-100" value={column.name} onChange={(evt) => changeColumnNameAt(index, evt.target.value)}/>
+                  <input className="px-1 bg-gray-100" value={column.name} onChange={(evt) => changeColumnNameAt(index, evt.target.value)} />
                 </th>
               )
             })}
@@ -160,11 +157,100 @@ export const Table = observer(() => {
 })
 
 
+export const SpreadSheet = observer(() => {
+  const doc = textEditorStateMobx.get().doc
+
+  const sortedSnippets = getAllSortedSnippets(doc.sliceString(0))
+
+
+  const [selectedFormulaIndex, setSelectedFormulaIndex] = useState<number | undefined>(undefined)
+
+  const [columns, setColumns] = useState<FormulaColumn[]>([
+    {
+      name: 'workouts',
+      formula: 'VALUES_OF_TYPE("number")'
+    }
+  ])
+
+  const rows: ResultRow[] = evaluateColumns(columns, sortedSnippets, doc)
+
+  const changeColumnNameAt = (changedIndex: number, name: string) => {
+    setColumns(columns.map((column, index) => (
+      index === changedIndex ? { ...column, name } : column
+    )))
+  }
+
+
+  const changeFormulaAt = (changedIndex: number, formula: string) => {
+    setColumns(columns.map((column, index) => (
+      index === changedIndex ? { ...column, formula } : column
+    )))
+  }
+
+
+  return (
+    <div className="flex flex-col gap-2 flex-1">
+
+      {selectedFormulaIndex !== undefined && (
+        <input
+          className="border border-gray-200 w-f"
+          value={columns[selectedFormulaIndex].formula}
+          onChange={(evt) => changeFormulaAt(selectedFormulaIndex, evt.target.value)}
+        />
+      )}
+
+      <table>
+        <thead>
+          <tr>
+            {columns.map((column, index) => {
+              return (
+                <th
+                  key={index}
+                  className={`bg-gray-100 border ${selectedFormulaIndex === index ? 'border-blue-300' : 'border-gray-200'}`}
+                  onClick={() => setSelectedFormulaIndex(index)}
+                >
+                  {column.name}
+
+                  {false && <input className="bg-gray-100" value={column.name} onChange={(evt) => changeColumnNameAt(index, evt.target.value)} />}
+                </th>
+              )
+            })}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.map((row) => (
+            <tr>
+              {columns.map((column) => {
+                const value: any = row[column.name]
+
+                let displayValue = JSON.stringify(value);
+
+                if (value.span && value.type) {
+                  const text = doc.sliceString(value.span[0], value.span[1])
+                  const parser = getParserOfType(value.type)
+                  displayValue = <span className={parser!.color}>{text}</span>
+                }
+
+                return (
+                  <td className="border border-gray-200 px-1">
+                    {displayValue}
+                  </td>
+                )
+              })}
+            </tr>
+          ))}
+
+        </tbody>
+      </table>
+    </div>
+  )
+})
+
 function App() {
   return (
     <div className="flex p-4 gap-4 items-start">
-      <Editor/>
-      <Table/>
+      <Editor />
+      <SpreadSheet />
     </div>
   )
 }
--- src/formulas.ts
+++ src/formulas.ts
@@ -1,53 +1,83 @@
 import { Snippet } from "./primitives";
-import { curry, isFunction } from "lodash";
+import { curry, isFunction, isArray } from "lodash";
 import { Text } from "@codemirror/state";
+import { nanoid } from "nanoid";
 
 
-export function run(snippets: Snippet[], doc: Text) {
+export type FormulaColumn = {
+  name: string,
+  formula: string
+}
+
+export type ResultRow = {[name: string]: any}
+
+function evaluateFormula (source: string, snippets: Snippet[], doc: Text) {
+
+  const API = {
+    VALUES_OF_TYPE: (type: string): Snippet[] => {
+      return snippets.filter((snippet) => snippet.type === type)
+    },
+
+    ON_SAME_LINE: curry((a: Snippet, b: Snippet): boolean => {
+      const lineStartA = doc.lineAt(a.span[0]).number
+      const lineEndA = doc.lineAt(a.span[1]).number
+      const lineStartB = doc.lineAt(b.span[0]).number
+      const lineEndB = doc.lineAt(b.span[1]).number
+
+      return (
+        lineStartA === lineEndA &&
+        lineStartB === lineEndB &&
+        lineStartA === lineStartB
+      )
+    }),
+
+    FILTER: curry((list: any[], condition: any): any[] => {
+      return list.filter((item: any) => {
+        if (isFunction(condition)) {
+
+          return condition(item)
+        }
+        return item
+      })
+    }),
+
+    FIRST: (list: any[]): any => {
+      return list[0]
+    },
 
 
-  const VALUES_OF_TYPE = (type: string): Snippet[] => {
-    return snippets.filter((snippet) => snippet.type === type)
+    SECOND: (list: any[]): any => {
+      return list[1];
+    }
   }
 
-  const ON_SAME_LINE = curry((a: Snippet, b: Snippet): boolean => {
-    const lineStartA = doc.lineAt(a.span[0]).number
-    const lineEndA = doc.lineAt(a.span[1]).number
-    const lineStartB = doc.lineAt(b.span[0]).number
-    const lineEndB = doc.lineAt(b.span[1]).number
+  let fn = new Function('API', `
+    with (API) {
+      return ${source}
+    }
+  `)
 
-    return (
-      lineStartA === lineEndA &&
-      lineStartB === lineEndB &&
-      lineStartA === lineStartB
-    )
-  })
+  return fn(API)
+}
+
+export function evaluateColumns (columns: FormulaColumn[], snippets: Snippet[], doc: Text) : ResultRow[]  {
+  let resultRows : ResultRow[] = [];
 
 
-  const FILTER = curry((list: any[], condition: any): any[] => {
-    return list.filter((item: any) => {
-      if (isFunction(condition)) {
+  for (const column of columns) {
+    if (resultRows.length === 0) {
+      const result = evaluateFormula(column.formula, snippets, doc)
 
-        return condition(item)
+      if (isArray(result)) {
+        for (const item of result) {
+          resultRows.push({ [column.name]: item })
+        }
+
+      } else {
+        resultRows.push({ [column.name]: result })
       }
-      return item
-    })
-  })
-
-  const FIRST = (list: any[]) : any => {
-    return list[0]
+    }
   }
 
-
-  const SECOND = (list: any[]) : any => {
-    return list[1];
-  }
-
-
-  const firstNumber =   FIRST(VALUES_OF_TYPE('number'))
-  const second =   SECOND(VALUES_OF_TYPE('number'))
-
-
-  console.log('numbers', FILTER(VALUES_OF_TYPE('number'), ON_SAME_LINE(FIRST(VALUES_OF_TYPE('number')))))
-
+  return resultRows
 }

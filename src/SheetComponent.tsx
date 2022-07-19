308ad6bdb9a4950680cd1e6fe69f02ee828ed009
Paul Shen <paulshen0@gmail.com>
2022-07-19 12:52:01 -0700

    move component into SheetTable

--- src/SheetComponent.tsx
+++ src/SheetComponent.tsx
@@ -5,7 +5,8 @@ import { action, comparer, computed, runInAction } from "mobx";
 import { observer } from "mobx-react-lite";
 import { useState } from "react";
 import {
-  hoverHighlightsMobx, isSheetExpandedMobx,
+  hoverHighlightsMobx,
+  isSheetExpandedMobx,
   SheetConfig,
   sheetConfigsMobx,
   SheetValueRow,
@@ -14,11 +15,11 @@ import {
   textEditorStateMobx,
 } from "./primitives";
 import { doSpansOverlap } from "./utils";
+import { FormulaColumn } from "./formulas";
 
 let i = 1;
 
 function ValueDisplay({ value, doc }: { value: any; doc: Text }) {
-
   if (value instanceof Error) {
     return <span className="text-red-500">#Err</span>;
   }
@@ -74,27 +75,37 @@ const textEditorSelectionSpanComputed = computed<Span>(() => {
   return [selectionRange.from, selectionRange.to] as Span;
 });
 
-export const SheetComponent = observer(
+export const SheetTable = observer(
   ({
-    id,
     textDocument,
-    sheetConfigId,
+    sheetConfig,
+    columns,
     rows,
   }: {
-    id: string,
     textDocument: TextDocument;
-    sheetConfigId: string;
+    sheetConfig: SheetConfig;
+    columns: FormulaColumn[];
     rows: SheetValueRow[];
   }) => {
-    const doc = textDocument.text;
-    const textDocumentSheet = textDocument.sheets.find(
-      (sheet) => sheet.configId === sheetConfigId
-    )!;
-
     const [selectedFormulaIndex, setSelectedFormulaIndex] = useState<number>(0);
+    const hoverHighlights = computed(
+      () =>
+        rows.filter(
+          (row) =>
+            "span" in row &&
+            row.span !== undefined &&
+            doSpansOverlap(row.span, textEditorSelectionSpanComputed.get())
+        ),
+      { equals: comparer.shallow }
+    ).get();
 
-    const sheetConfig = sheetConfigsMobx.get(sheetConfigId)!;
-    const columns = sheetConfig.columns;
+    const addColumn = action(() => {
+      sheetConfig.columns.push({
+        name: `col${++i}`,
+        formula: "",
+      });
+      setSelectedFormulaIndex(columns.length - 1);
+    });
 
     const changeFormulaAt = action((changedIndex: number, formula: string) => {
       sheetConfig.columns = sheetConfig.columns.map((column, index) =>
@@ -108,150 +119,168 @@ export const SheetComponent = observer(
       );
     });
 
-    const addColumn = action(() => {
-      sheetConfig.columns.push({
-        name: `col${++i}`,
-        formula: "",
-      });
-      setSelectedFormulaIndex(columns.length - 1);
-    });
+    return (
+      <>
+        {selectedFormulaIndex !== undefined && (
+          <div className="flex mr-[30px]">
+            <input
+              className="pl-1 border border-gray-200"
+              value={columns[selectedFormulaIndex].name}
+              onChange={(evt) =>
+                changeNameAt(selectedFormulaIndex, evt.target.value)
+              }
+            />
+            <span>&nbsp;=&nbsp;</span>
+            <input
+              className="pl-1 border border-gray-200 flex-1"
+              value={columns[selectedFormulaIndex].formula}
+              onChange={(evt) =>
+                changeFormulaAt(selectedFormulaIndex, evt.target.value)
+              }
+            />
+          </div>
+        )}
 
-    const hoverHighlights = computed(
-      () =>
-        rows.filter(
-          (row) =>
-            "span" in row &&
-            row.span !== undefined &&
-            doSpansOverlap(row.span, textEditorSelectionSpanComputed.get())
-        ),
-      { equals: comparer.shallow }
-    ).get();
+        <table className="max-h-[200px] overflow-auto">
+          <thead>
+            <tr>
+              {columns.map((column, index) => {
+                return (
+                  <th
+                    key={index}
+                    className={`text-left font-normal px-1 bg-gray-100 border ${
+                      selectedFormulaIndex === index
+                        ? "border-blue-300"
+                        : "border-gray-200"
+                    }`}
+                    onClick={() => setSelectedFormulaIndex(index)}
+                  >
+                    {column.name}
+                  </th>
+                );
+              })}
+              <th className="w-[30px]">
+                <button
+                  className="icon icon-plus bg-gray-500"
+                  onClick={() => addColumn()}
+                />
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {rows.map((row, index) => (
+              <tr
+                onMouseEnter={action(() => {
+                  if ("span" in row && row.span !== undefined) {
+                    hoverHighlightsMobx.replace([row]);
+                  }
+                })}
+                onMouseLeave={action(() => {
+                  hoverHighlightsMobx.clear();
+                })}
+                className={classNames(
+                  "hover:bg-blue-50",
+                  hoverHighlights.includes(row) ? "bg-blue-100" : undefined
+                )}
+                key={index}
+              >
+                {columns.map((column, index) => {
+                  const value: any = row.data[column.name];
+                  return (
+                    <td className="border border-gray-200 px-1" key={index}>
+                      <ValueDisplay value={value} doc={textDocument.text} />
+                    </td>
+                  );
+                })}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+);
 
+export const SheetComponent = observer(
+  ({
+    id,
+    textDocument,
+    sheetConfigId,
+    rows,
+  }: {
+    id: string;
+    textDocument: TextDocument;
+    sheetConfigId: string;
+    rows: SheetValueRow[];
+  }) => {
+    const textDocumentSheet = textDocument.sheets.find(
+      (sheet) => sheet.configId === sheetConfigId
+    )!;
 
-    const isExpanded = isSheetExpandedMobx.get(id)
+    const sheetConfig = sheetConfigsMobx.get(sheetConfigId)!;
+    const columns = sheetConfig.columns;
+
+    const isExpanded = isSheetExpandedMobx.get(id);
 
     const toggleIsExpanded = action(() => {
-      isSheetExpandedMobx.set(id, !isExpanded)
-    })
+      isSheetExpandedMobx.set(id, !isExpanded);
+    });
 
     return (
       <div className="flex flex-col gap-2 flex-1">
         <div className="flex gap-1">
           <button onClick={() => toggleIsExpanded()}>
-            <span className={`icon icon-expandable bg-gray-500 ${isExpanded ? 'is-expanded' : ''}`} />
+            <span
+              className={`icon icon-expandable bg-gray-500 ${
+                isExpanded ? "is-expanded" : ""
+              }`}
+            />
           </button>
 
           <SheetName sheetConfig={sheetConfig} />
         </div>
 
-        {isExpanded && (<>
-          <div className="text-sm text-gray-500">
-            Highlighting in:{" "}
-            {textDocumentSheet.highlightSearchRange === undefined
-              ? "whole document"
-              : `chars ${textDocumentSheet.highlightSearchRange[0]} to ${textDocumentSheet.highlightSearchRange[1]}`}
-            <button
-              className="ml-4"
-              onClick={() =>
-                runInAction(() => {
-                  textDocumentSheet.highlightSearchRange = undefined;
-                })
-              }
-            >
-              Clear
-            </button>
-            <button
-              className="ml-4"
-              onClick={() =>
-                runInAction(() => {
-                  const editorState = textEditorStateMobx.get();
-                  const from = editorState.selection.main.from;
-                  const to = editorState.selection.main.to;
-                  if (from !== to) {
-                    textDocumentSheet.highlightSearchRange = [from, to];
-                  }
-                })
-              }
-            >
-              Update
-            </button>
-          </div>
-          {selectedFormulaIndex !== undefined && (
-            <div className="flex mr-[30px]">
-              <input
-                className="pl-1 border border-gray-200"
-                value={columns[selectedFormulaIndex].name}
-                onChange={(evt) =>
-                  changeNameAt(selectedFormulaIndex, evt.target.value)
+        {isExpanded && (
+          <>
+            <div className="text-sm text-gray-500">
+              Highlighting in:{" "}
+              {textDocumentSheet.highlightSearchRange === undefined
+                ? "whole document"
+                : `chars ${textDocumentSheet.highlightSearchRange[0]} to ${textDocumentSheet.highlightSearchRange[1]}`}
+              <button
+                className="ml-4"
+                onClick={() =>
+                  runInAction(() => {
+                    textDocumentSheet.highlightSearchRange = undefined;
+                  })
                 }
-              />
-              <span>&nbsp;=&nbsp;</span>
-              <input
-                className="pl-1 border border-gray-200 flex-1"
-                value={columns[selectedFormulaIndex].formula}
-                onChange={(evt) =>
-                  changeFormulaAt(selectedFormulaIndex, evt.target.value)
-                }
-              />
-            </div>
-          )}
-
-          <table className="max-h-[200px] overflow-auto">
-            <thead>
-              <tr>
-                {columns.map((column, index) => {
-                  return (
-                    <th
-                      key={index}
-                      className={`text-left font-normal px-1 bg-gray-100 border ${
-                        selectedFormulaIndex === index
-                          ? "border-blue-300"
-                          : "border-gray-200"
-                      }`}
-                      onClick={() => setSelectedFormulaIndex(index)}
-                    >
-                      {column.name}
-                    </th>
-                  );
-                })}
-                <th className="w-[30px]">
-                  <button
-                    className="icon icon-plus bg-gray-500"
-                    onClick={() => addColumn()}
-                  />
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {rows.map((row, index) => (
-                <tr
-                  onMouseEnter={action(() => {
-                    if ("span" in row && row.span !== undefined) {
-                      hoverHighlightsMobx.replace([row]);
+              >
+                Clear
+              </button>
+              <button
+                className="ml-4"
+                onClick={() =>
+                  runInAction(() => {
+                    const editorState = textEditorStateMobx.get();
+                    const from = editorState.selection.main.from;
+                    const to = editorState.selection.main.to;
+                    if (from !== to) {
+                      textDocumentSheet.highlightSearchRange = [from, to];
                     }
-                  })}
-                  onMouseLeave={action(() => {
-                    hoverHighlightsMobx.clear();
-                  })}
-                  className={classNames(
-                    "hover:bg-blue-50",
-                    hoverHighlights.includes(row) ? "bg-blue-100" : undefined
-                  )}
-                  key={index}
-                >
-                  {columns.map((column, index) => {
-                    const value: any = row.data[column.name];
-                    return (
-                      <td className="border border-gray-200 px-1" key={index}>
-                        <ValueDisplay value={value} doc={doc} />
-                      </td>
-                    );
-                  })}
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </>)}
+                  })
+                }
+              >
+                Update
+              </button>
+            </div>
+            <SheetTable
+              textDocument={textDocument}
+              sheetConfig={sheetConfig}
+              columns={columns}
+              rows={rows}
+            />
+          </>
+        )}
       </div>
     );
   }

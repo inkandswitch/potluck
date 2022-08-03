b3dae7ec18fa14be3e10ed2420065cf7b23599fd
Max Schoening <max@max.dev>
2022-08-03 15:26:07 -0700

    Ignore sample-data for live reloading

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,25 @@
-import { defineConfig } from "vite";
+import { defineConfig, ViteDevServer } from "vite";
 import react from "@vitejs/plugin-react";
 import dsv from "@rollup/plugin-dsv";
 
+// The Vite Chokidar overrides don't actually work unless you write a plugin.
+// This plugin overwrites the `ignored` directories for live-reloading. We do
+// this to avoid realods on changes to `sample-data/`.
+//
+// https://github.com/vitejs/vite/issues/8341
+const ignored = () => {
+  return {
+    name: "watch-node-modules",
+    configureServer: (server: ViteDevServer): void => {
+      server.watcher.options = {
+        ...server.watcher.options,
+        ignored: ["**/.git/**", "**/node_modules/**", "**/sample-data/**"],
+      };
+    },
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dsv()],
+  plugins: [react(), dsv(), ignored()],
 });

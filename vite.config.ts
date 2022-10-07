a88218b9245430e72a9b771e0727327d22b9b215
Paul Sonnentag <paul.sonnentag@googlemail.com>
2022-10-07 10:42:43 +0200

    Configure build
    - use relative paths
    - split bundles into embedded and main

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, ViteDevServer } from "vite";
 import react from "@vitejs/plugin-react";
 import dsv from "@rollup/plugin-dsv";
+import { resolve } from 'path';
 
 // The Vite Chokidar overrides don't actually work unless you write a plugin.
 // This plugin overwrites the `ignored` directories for live-reloading. We do
@@ -22,7 +23,16 @@ const ignored = () => {
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), dsv(), ignored()],
+  base: '',
   server: {
     host: true
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        embedded: resolve(__dirname, "embedded.html")
+      }
+    }
   }
 });

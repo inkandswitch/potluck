e18a0bcf16923de0562a66e1746fe54e1be14462
Paul Sonnentag <paul.sonnentag@googlemail.com>
2022-10-10 09:19:46 +0200

    Allow to link to specific documents with openDocument parameter

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,15 @@ import App from "./App";
 import * as Tooltip from "@radix-ui/react-tooltip";
 import * as Toast from "@radix-ui/react-toast";
 import "./index.css";
+import { selectedTextDocumentIdBox, textDocumentsMobx } from "./primitives";
+
+const openDocumentId = new URLSearchParams(location.search).get("openDocument")
+
+
+if (openDocumentId && textDocumentsMobx.get(openDocumentId)) {
+  selectedTextDocumentIdBox.set(openDocumentId)
+}
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
